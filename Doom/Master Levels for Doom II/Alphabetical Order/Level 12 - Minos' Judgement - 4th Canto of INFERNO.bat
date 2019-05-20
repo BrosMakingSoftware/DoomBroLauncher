da9fb111f63c5d18c03a4a8c88a2dd3d5ad29df6
iampeluca <diego.hernandez.perez.cr@gmail.com>
2019-05-19 20:50:03 -0600

    Added bat scripts for running Master Levels for Doom II. Also added better box arts and created an improved Ultimate Doom cover and added Doom logos on some map screenshots.

---
+++ Doom/Master Levels for Doom II/Alphabetical Order/Level 12 - Minos' Judgement - 4th Canto of INFERNO.bat
@@ -0,0 +1,15 @@
+REM ---------- Script Template ----------
+REM Moving our current location to the script's location
+cd %~dp0
+REM Now let's move to our working folder
+cd ../../.DoomBroLauncher
+REM With this script we set the common parameters for the engine in a variable
+call config/port-parameters.bat
+REM ---------- End of Script Template ----------
+
+
+REM Now on our working folder, we call the port with our wads and common parameters
+REM Master Levels for Doom II
+REM Map: MAP05
+REM Title: Minos' Judgement: 4th Canto of INFERNO
+%PORT_EXECUTABLE% %PORT_PARAMETERS% -iwad wads/DOOM2.WAD -file wads/master_levels/MINOS.WAD +map MAP05 %SKILL_DEFAULT%
